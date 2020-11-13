f1ef3425d06d8c983188ab40984e26c25fc6278c
robertstreche <robert.streche@uipath.com>
2020-11-13 17:22:52 +0200

    Added localization for properties and tasks
    Added localization for the html help files
    WIP - add the translations
    
    Updated the help files logic
    
    Added html help files generator
    
    Updated the gitignore to exclude html files

---
+++ GenerateHelpFiles.ps1
@@ -0,0 +1,50 @@
+$OpenDivTag='<div>'
+$CloseDivTag='</div>'
+
+$ResourcesRelativePath = "src\\main\\resources\\com\\uipath\\uipathpackage\\"
+$FolderName = "help"
+$ActivitiesFolders = @{"TokenAuthenticationEntry" = "entries\\authentication\\TokenAuthenticationEntry";
+                        "DynamicallyEntry" = "entries\\job\\DynamicallyEntry";
+                        "RobotEntry" = "entries\\job\RobotEntry";
+                        "TestProjectEntry" = "entries\\testExecutionTarget\\TestProjectEntry";
+                        "TestSetEntry" = "entries\\testExecutionTarget\\TestSetEntry";
+                        "UiPathDeploy" = "UiPathDeploy";
+                        "UiPathPack" = "UiPathPack";
+                        "UiPathRunJob" = "UiPathRunJob";
+                        "UiPathTest" = "UiPathTest"}
+
+$Languages = @("", "_de_DE", "_es", "_es_MX", "_fr", "_ja", "_ko", "_pt", "_pt_BR", "_ru", "_tr", "_zh_CN")
+
+# Set content of a file using FilePath and Value
+function CreateFileWithContent {
+    param (
+        $FilePath,
+        $Value
+    )
+
+    $TemplateValue = $OpenDivTag + $Value + $CloseDivTag
+    Set-Content -Path $FilePath -Value $TemplateValue -Force
+}
+
+# Create the html help files for jelly interfaces
+function CreateHelpFiles {
+    # Iterate through help.properties files to create help.html files for each language
+    foreach ($language in $Languages) {
+        $content = Get-Content $ResourcesRelativePath\help$language.properties -Force
+
+        foreach ($line in $content) {
+            # Get the key-value pairs and create a html file for each entry in the properties file
+            $model, $key = $line.Split('=')[0].Split('.')
+            $value = $line.Split('=')[1]
+
+            $subfolder = $ActivitiesFolders[$model]
+
+            $FileName = "$FolderName-$key$language.html"
+            $FilePath = ".\$ResourcesRelativePath\$subfolder\$Filename"
+
+            CreateFileWithContent -FilePath $FilePath -Value $value
+        }
+    }
+}
+
+CreateHelpFiles
