07e4a56df70f2a6535d52efcde82ccf6b333d879
rishi-104276 <rishi.singh@uipath.com>
2021-08-24 14:01:46 +0530

    [TS-1624] | added missed import

--- src/main/java/com/uipath/uipathpackage/UiPathTest.java
+++ src/main/java/com/uipath/uipathpackage/UiPathTest.java
@@ -22,6 +22,8 @@ import hudson.util.FormValidation;
 import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
 import jenkins.tasks.SimpleBuildStep;
+
+import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
