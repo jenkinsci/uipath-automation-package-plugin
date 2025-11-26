ceea4fedc70668a64567b8aef455f1bc79048347
AndreiDinca24 <andrei.dinca@uipath.com>
2025-11-26 14:20:38 +0200

    Jenkins: support test manager + update default uipcli versions [STUD-78350]

--- src/main/java/com/uipath/uipathpackage/configuration/UiPathCliConfiguration.java
+++ src/main/java/com/uipath/uipathpackage/configuration/UiPathCliConfiguration.java
@@ -194,9 +194,9 @@ public final class UiPathCliConfiguration {
 
         // Extract platform and version from CLI package name
         // Pattern: UiPath.CLI.{Platform}.{Version}
-        // Platform can be: Windows, Windows.Legacy, Linux, Mac
+        // Platform can be: Windows, Windows.Legacy, Linux, macOS
         // Version can be: 25.10.0-20251017-19 or 25.10.9424.14050
-        Pattern packageRegex = Pattern.compile("^UiPath\\.CLI\\.(Windows\\.Legacy|Windows|Linux|Mac)\\.((?:\\d+\\.\\d+\\.\\d+(?:\\.\\d+)*)-(?:\\d+(?:-\\d+)*)|\\d+\\.\\d+\\.\\d+(?:\\.\\d+)*)$");
+        Pattern packageRegex = Pattern.compile("^UiPath\\.CLI\\.(Windows\\.Legacy|Windows|Linux|macOS)\\.((?:\\d+\\.\\d+\\.\\d+|\\d+\\.\\d+\\.\\d+\\.\\d+|\\d+\\.\\d+\\.\\d+\\.\\d+\\.\\d+)(?:-[A-Za-z0-9-]+|))$");
         Matcher match = packageRegex.matcher(packageName);
 
         if (!match.matches()) {
